54c4222e47ce3a02c863828b7c36c1c19ae2fa0b
vinodanbalagan <vinodanbalagan@gmail.com>
2024-07-22 23:39:43 -0400

    Refactor: Changed input statment for readability

--- bikeshare.py
+++ bikeshare.py
@@ -17,7 +17,7 @@ def get_filters():
     print('Hello! Let\'s explore some US bikeshare data!')
     # get user input for city (chicago, new york city, washington). HINT: Use a while loop to handle invalid inputs
     while True:
-        city = input('Would you like to analyze data for Chicago, New York City, or Washington? ').lower()
+        city = input('Would you like to analyze bike share data for Chicago, New York City, or Washington? ').lower()
         if city in CITY_DATA.keys():
             break
         else:
